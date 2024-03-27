d0859a4c47e6afe12228ac2452cf3fb8d87c2b86
Jhon Fabio Cardona Martinez <programasjhonfa@gmail.com>
2024-03-26 23:22:16 -0500

    Slug Eloquent

--- app/Models/Wine.php
+++ app/Models/Wine.php
@@ -5,9 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Category extends Model
+class Wine extends Model
 {
     use HasFactory;
-
-    
 }
